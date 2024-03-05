6fe98e111757a3be9cb3af525bb9a495a338f4d0
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2024-03-05 18:14:26 +0200

    init : Main models

---
+++ core/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Doctor(models.Model):
+    pass
+    
+class Student(models.Model):
+    pass
+
+class Level(models.Model):
+    pass
+
+class Subject(models.Model):
+    pass
+
+class Department(models.Model):
+    pass
+
+class Attendance(models.Model):
+    pass
+
