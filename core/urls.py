77994a74a8b83c1bf003e6feea8d7f272a9569c2
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2024-03-09 09:45:04 +0200

    .

--- core/urls.py
+++ core/urls.py
@@ -3,12 +3,3 @@ from django.urls import path
 
 
 
-from .views import record_attendance
-
-
-
-urlpatterns = [
-    #  path('',show_record),
-     path('record_attendance/', record_attendance, name='record_attendance'),
-
-]
