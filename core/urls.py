a8aaf2aaa9a08bbeb1a9904cee2079bc79b2ec42
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2024-04-17 01:33:37 +0200

    finish attendance lecture

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
 #     path('students/<int:subject_id>/', views.student_list, name='student-list'),
 #     path('attendance/add/', views.AttendanceCreateView.as_view(), name='add-attendance'),
-path('lec_attendance/', views.lec_attendance, name='lec_attendance'),
-path('submit_attendance/', views.submit_attendance, name='submit_attendance'),
+path('lec_attendance/<int:subject_id>', views.lec_attendance, name='lec_attendance'),
+path('submit_attendance/<int:subject_id>', views.submit_attendance, name='submit_attendance'),
 path('subjects/', views.subjects_by_level, name='subjects_by_level'),
 ]   
--- core/views.py
+++ core/views.py
@@ -1,48 +1,64 @@
-from django.shortcuts import render
+from datetime import date
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Doctor, Student, Attendance, Subject, Level
 from django.contrib.auth.decorators import login_required
-from collections import defaultdict
-from django.urls import reverse
-
-from django.shortcuts import render, redirect
-from .models import Doctor, Student
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
+from django.urls import reverse 
+from django.http import HttpResponseRedirect,HttpResponse
+from django.contrib import messages
 
 @login_required
-def lec_attendance(request):
+def lec_attendance(request, subject_id=None):
     if not request.user.is_doctor:
-      
         return redirect('/')
-
   
-    doctor = request.user.doctor_user 
+    doctor = request.user.doctor_user
     students = Student.objects.filter(department=doctor.department, level__in=doctor.level.all()).distinct()
-    return render(request, 'attendance/tae.html', {'students': students})
     
-    
+    if subject_id:
+        subject = get_object_or_404(Subject, id=subject_id)
+        return render(request, 'attendance/lec_attendance.html', {'students': students, 'subject': subject})
+    else:
+        subjects = Subject.objects.filter(level__in=doctor.level.all())
+        return render(request, 'attendance/lec_attendance.html', {'students': students, 'subjects': subjects})
 
 @login_required
-def submit_attendance(request):
+def submit_attendance(request, subject_id):
     if request.method == 'POST':
+        
+        subject = get_object_or_404(Subject, id=subject_id)
         student_ids = request.POST.getlist('student_ids')
-        print('<<<<<<<<<<<<<<<<<<<<<<<<<<')
+        absent_students = []  
+        already_recorded = False  
+        
         for student_id in student_ids:
-            attendance_status = request.POST.get(f'attendance_{student_id}', 'absent') 
-            student = Student.objects.get(idc=student_id)
-            print('attttttttttttttttttttttttttttt')
-            Attendance.objects.update_or_create(
+            attendance_status = request.POST.get(f'attendance_{student_id}', 'absent')
+            student = get_object_or_404(Student, idc=student_id)
+            
+            if Attendance.objects.filter(student=student, subject=subject, date=date.today()).exists():
+                already_recorded = True
+                continue  
+            
+            Attendance.objects.create(
                 student=student,
-                defaults={'status': attendance_status == 'present'}
+                subject=subject,
+                status=attendance_status == 'present'
             )
-            print('doneeeeeeeeeeee')
-
-        return HttpResponseRedirect(reverse('lec_attendance'))
+            
+            if attendance_status == 'absent':
+                absent_students.append(student.name)  
+        
+        if already_recorded:
+            messages.warning(request, 'Attendance for some students was already recorded today.')
+        
+        if absent_students:
+            message = f'Number of absent students: {len(absent_students)}. Absent students: {", ".join(absent_students)}.'
+            messages.success(request, message)
+        else:
+            messages.success(request, 'All students are marked present.')
+        
+        return HttpResponseRedirect(reverse('lec_attendance', args=[subject_id]))
     else:
-       
-        return HttpResponseRedirect(reverse('lec_attendance'))
-        
-        
+        return redirect('subjects_by_level')
         
 @login_required
 def subjects_by_level(request):
@@ -60,3 +76,9 @@ def subjects_by_level(request):
     subjects_by_level = {level: [subject for subject in subjects if subject.level == level] for level in levels}
 
     return render(request, 'subjects/subject_list.html', {'subjects_by_level': subjects_by_level})
+    
+    
+    
+    
+
+
