a5caffea61c89894db8711804d5ff0b54ef7b063
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2024-04-18 01:15:55 +0200

    buff : prevent admin to update attendance model

--- core/admin.py
+++ core/admin.py
@@ -8,14 +8,13 @@ admin.site.register(Doctor)
 admin.site.register(Subject)
 admin.site.register(Department)
 admin.site.register(Level)
-admin.site.register(Attendance)
 
-# @admin.register(Attendance)
-# # class AttendanceAdmin(admin.ModelAdmin):
-# #     def save_model(self, request, obj, form, change):
-# #         # Check if an attendance record for this student, subject, and date already exists
-# #         if Attendance.objects.filter(student=obj.student, subject=obj.subject, date=date.today()).exists():
-# #             # Prevent saving and inform the user
-# #             messages.error(request, "Attendance for this student and subject has already been recorded today.")
-# #         else:
-# #             super().save_model(request, obj, form, change)
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        
+        if Attendance.objects.filter(student=obj.student, subject=obj.subject, date=date.today()).exists():
+             
+            messages.error(request, "Attendance for this student and subject has already been recorded today.")
+        else:
+            super().save_model(request, obj, form, change)
