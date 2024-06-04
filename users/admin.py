936993235c9036fb7d7c6d87ff78f4d41003a13e
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2024-06-04 20:19:05 +0300

    fix issue : hash passwords

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,22 @@
- 
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import User
-from django.shortcuts import redirect
-from django.contrib import admin,messages
-from core.models import Student, Doctor, Subject, Department, Level, Attendance,TimeTable
+from django.contrib.auth.hashers import make_password
+from core.models import Student, Doctor, Subject, Department, Level, Attendance, TimeTable
 from datetime import date
 from .admin_site import admin_site  # Import the custom admin site
+
+class StudentFilter(admin.SimpleListFilter):
+    title = 'student'
+    parameter_name = 'student'
+
+    def lookups(self, request, model_admin):
+        students = set([a.student for a in Attendance.objects.all()])
+        return [(s.idc, s.name) for s in students]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(student__idc=self.value())
+        return queryset
 
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'is_student', 'is_doctor', 'is_staff')
@@ -16,9 +27,26 @@ class CustomUserAdmin(admin.ModelAdmin):
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
         ('Roles', {'fields': ('is_student', 'is_doctor')}),
     )
-
+    def save_model(self, request, obj, form, change):
+        if 'password' in form.changed_data:
+            obj.password = make_password(obj.password)
+        super().save_model(request, obj, form, change)
 admin_site.register(User, CustomUserAdmin)
 
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('student', 'subject', 'date', 'status', 'count_attendance')
+    list_filter = (StudentFilter, 'subject', 'date', 'status')
+    search_fields = ('student__name', 'subject__sname')
+    
+    def save_model(self, request, obj, form, change):
+        if Attendance.objects.filter(student=obj.student, subject=obj.subject, date=date.today()).exists():
+            messages.error(request, "Attendance for this student and subject has already been recorded today.")
+        else:
+            super().save_model(request, obj, form, change)
+
+    def count_attendance(self, obj):
+        return Attendance.objects.filter(student=obj.student, subject=obj.subject,status = True).count()
+    count_attendance.short_description = 'Total Attendance for Subject'
 
 # Register your models here.
 admin_site.register(Student)
@@ -27,13 +55,4 @@ admin_site.register(Subject)
 admin_site.register(Department)
 admin_site.register(Level)
 admin_site.register(TimeTable)
-
-@admin.register(Attendance)
-class AttendanceAdmin(admin.ModelAdmin):
-    def save_model(self, request, obj, form, change):
-        
-        if Attendance.objects.filter(student=obj.student, subject=obj.subject, date=date.today()).exists():
-             
-            messages.error(request, "Attendance for this student and subject has already been recorded today.")
-        else:
-            super().save_model(request, obj, form, change)
+admin_site.register(Attendance, AttendanceAdmin)  # Register Attendance with AttendanceAdmin
